2c3f1500ac77cb7132052fa8c292006995e74b21
arturofburgos <burgos.arturo.ufu@gmail.com>
2024-07-01 18:23:01 -0500

    Add new module FronteiraImersa

---
+++ src/FronteiraImersa.jl
@@ -0,0 +1,13 @@
+module FronteiraImersa
+
+include("pre-processing/include_pre_processing.jl")
+include("fluid-domain/include_fluid_domain.jl")
+
+export ReadUserVars
+export MakeGrid
+
+# dx, freestream, dt, T = read_user_vars(freestream, dx, dt, Re, T)
+
+
+
+end
