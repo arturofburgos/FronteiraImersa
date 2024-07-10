cbaae0b502c43ddf5ebd846173d4d5b4cb4555ed
arturofburgos <burgos.arturo.ufu@gmail.com>
2024-07-10 18:30:47 -0500

    Change in the include order. It was wrong before.

--- src/FronteiraImersa.jl
+++ src/FronteiraImersa.jl
@@ -3,11 +3,12 @@ module FronteiraImersa
 using LinearMaps
 using Plots
 
-include("fluid-operators/include_fluid_operators.jl")
-include("pre-processing/include_pre_processing.jl")
+
 include("fluid-domain/include_fluid_domain.jl")
 include("structure-domain/include_structure_domain.jl")
 include("interface-coupling/include_interface_coupling.jl")
+include("pre-processing/include_pre_processing.jl")
+include("fluid-operators/include_fluid_operators.jl")
 
 export ReadUserVars
 export MakeGrid
